893350f1cfb5af0159d536b29e9b09d1257532d1
Anwell Tsai <Anwell@playfultunes.com>
2018-02-26 13:57:28 -0500

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Anwell Tsai on 2/26/18.
+//  Copyright © 2018 Anwell Tsai. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
